15baba3eabe6b958ced0664f9c5dc6492ddeb8a8
Zahid Ghafoor <zahidghafoor40@gmail.com>
2021-06-05 11:38:04 +0500

    ToDo Page Modified

---
+++ build/precache-manifest.8cbe6097431d6b503a3a211e7e8629c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07de1f556fb43ddc9082505c98920281",
+    "url": "/ToDolist/index.html"
+  },
+  {
+    "revision": "5b9d8835f336cf63a5ef",
+    "url": "/ToDolist/static/css/main.a18b3fcf.chunk.css"
+  },
+  {
+    "revision": "314e2c30b0580f6d8836",
+    "url": "/ToDolist/static/js/2.4349a858.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/ToDolist/static/js/2.4349a858.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b9d8835f336cf63a5ef",
+    "url": "/ToDolist/static/js/main.51486290.chunk.js"
+  },
+  {
+    "revision": "5454ca4bd67cce5a2143",
+    "url": "/ToDolist/static/js/runtime-main.f3870dab.js"
+  }
+]);
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -57,3 +57,68 @@ const Todo = () => {
 };
 
 export default Todo;
+
+
+
+
+
+
+// import React, { useState } from "react";
+// import AddIcon from '@material-ui/icons/Add';
+// import List from './List';
+
+// const Todo = () => {
+//   const [item, setItem] = useState("");
+//   const [newitem, setNewItem] = useState([]);
+
+
+
+//   const itemEvent = (event) => {
+//     setItem(event.target.value);
+//   }
+
+//   const listofItems = () => {
+//     setNewItem((pvalue) => {
+//       return (
+//         [...pvalue, item]
+//       )
+//     });
+//     setItem("");
+//   };
+
+//   return (
+//     <>
+//       <div className="main_div">
+//         <div className="centre_div">
+//           <br />
+//           <h1>ZaHid's ToDo</h1>
+//           <br />
+//           <input value={item}
+//             type="text"
+//             placeholder="Add Your Items"
+//             onChange={itemEvent}
+//           />
+//           <button
+//             id="btn" variant="contained"
+//             onClick={listofItems}>
+//             <AddIcon />
+//           </button>
+//           <br />
+//           <ol>
+
+//             {newitem.map((val, index) => {
+//               return (
+//                 <List key={index} text={val} />
+//               )
+
+//             })}
+//           </ol>
+//           <br />
+//         </div>
+//       </div>
+
+//     </>
+//   )
+// };
+
+// export default Todo;
